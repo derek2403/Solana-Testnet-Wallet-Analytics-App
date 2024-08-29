14c2ce1372f548179e1d57a05f87d2173d6f463d
derek2403 <derekliew0@gmail.com>
2024-08-29 23:37:52 +0800

    nice

--- component/Background.js
+++ component/Background.js
@@ -35,8 +35,8 @@ const Background = () => {
         position: "fixed",
         top: 0,
         left: 0,
-        width: "100vw",
-        height: "100vh",
+        width: "100%",
+        height: "100%",
         backdropFilter: "blur(5px)",
       }}
     >
